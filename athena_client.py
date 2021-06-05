d6830780ac359922350a11cf7e8cd786f8dcfdec
abj <jainabhi24feb@gmail.com>
2021-06-05 16:27:09 +0530

    Refactored client and added unit tests

--- athena_client.py
+++ athena_client.py
@@ -1,11 +1,34 @@
 import requests
 
 
-print(requests.get("http://127.0.0.1:5001/greet/Alice").text)
-print(requests.get("http://127.0.0.1:5001/").text)
-print(requests.get("http://127.0.0.1:5001/square/8").text)
-requests.put("http://127.0.0.1:5001/todo/todo_1", data={"data": "Buy groceries"})
-print(requests.get("http://127.0.0.1:5001/todo/todo_1").text)
-print(requests.get("http://127.0.0.1:5001/todo/todo_2").text)
-requests.put("http://127.0.0.1:5001/todo/todo_2", data={"data": "Remember milk"})
-print(requests.get("http://127.0.0.1:5001/todo/todo_2").text)
+class GreetAPI:
+    def get_request(self, name):
+        try:
+            response = requests.get("http://127.0.0.1:5001/greet/%s" % name)
+        except:
+            raise Exception('')
+        return response
+
+
+class HelloAPI:
+    def get_request(self):
+        return requests.get("http://127.0.0.1:5001/")
+
+    def post_request(self, data):
+        return requests.post("http://127.0.0.1:5001/", data=data)
+
+
+class SquareAPI:
+    def get_square(self, num):
+        return requests.get("http://127.0.0.1:5001/square/8")
+
+
+class TodoAPI:
+    def get_task(self, task_id):
+        return requests.get("http://127.0.0.1:5001/todo/%s" % task_id)
+
+    def put_task(self, task_id, task):
+        return requests.put("http://127.0.0.1:5001/todo/%s" % task_id, data={"data": task})
+
+    def delete_task(self, task_id):
+        response = requests.delete("http://127.0.0.1:5001/tasks/%s" % task_id)
--- tests/test_athena_client.py
+++ tests/test_athena_client.py
@@ -1,1 +1,20 @@
-# test cases for athena API client
+import unittest
+import sys
+sys.path.append('..')
+
+from athena_client import GreetAPI, HelloAPI, SquareAPI, TodoAPI
+
+
+class GreetApiTestSuite(unittest.TestCase):
+    def setUp(self):
+        self.client = GreetAPI()
+
+    def test_get_request(self):
+        name = 'Abhi'
+        response = self.client.get_request(name)
+        self.assertEqual(response.text, "Hi %s!!!" % name)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
